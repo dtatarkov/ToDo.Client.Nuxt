da8cc2a49b82132a61c616d71a4e11881bfa28be
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-04-26 12:59:48 +0300

    Fixed ssr loader requests tracking

--- app/modules/shared/services/ssrLoaderImpl.ts
+++ app/modules/shared/services/ssrLoaderImpl.ts
@@ -8,18 +8,20 @@ enum SSRLoaderState
 
 export class SSRLoaderImpl extends SSRLoader
 {
-  protected state = SSRLoaderState.initial;
+  private _dataStates = new Map<string, SSRLoaderState>;
 
   override async loadAsync<T>(key: string, handler: () => Promise<T>): Promise<T>
   {
     let result: T;
 
-    if (this.state === SSRLoaderState.initial)
+    const state = this._dataStates.get(key) ?? SSRLoaderState.initial;
+
+    if (state === SSRLoaderState.initial)
     {
       const response = await useAsyncData(key, handler);
       result = response.data.value as T;
 
-      this.state = SSRLoaderState.initialized;
+      this._dataStates.set(key, SSRLoaderState.initialized);
     }
     else
     {
