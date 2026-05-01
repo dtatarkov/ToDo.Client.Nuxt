ff9ad0c2145bb2559cf01edd7ac146ef80c0d684
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-05-01 16:37:13 +0300

    storybook preview styles fixes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,13 +4,20 @@ const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-  
+
   tags: ['autodocs'],
+  decorators: [() => ({
+    template: `<div class="dark">
+        <div style="background-color: var(--ui-bg); color: var(--ui-text);">
+          <story/>
+        </div>
+      </div>`
+  })]
 };
 
 export default preview;
