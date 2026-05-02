8b92e80b3872298c803cf1cf6966ec62ac939ecb
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-05-02 17:01:27 +0300

    UIKitViewmodelsFactory interface fix

--- app/modules/uikit/interfaces/uikitViewmodelsFactory.ts
+++ app/modules/uikit/interfaces/uikitViewmodelsFactory.ts
@@ -1,5 +1,4 @@
 import type { ButtonGeneralViewmodel, ButtonGeneralViewmodelData, ButtonGeneralViewmodelHandlers } from './buttonGeneralViewmodel';
-import type { InputViewmodel } from './inputViewmodel';
 import type { GridViewmodel } from './gridViewmodel';
 import type { Viewmodel } from './viewmodel';
 import type { InfoBlockViewmodel } from './infoBlockViewmodel';
@@ -8,14 +7,18 @@ import type { CardViewmodel } from './cardViewmodel';
 import type { ToolbarViewmodel } from './toolbarViewmodel';
 import type { MaybeObservable } from '@/modules/shared/interfaces/maybeObservable';
 import type { InputTextViewmodel } from './inputTextViewmodel';
+import type { InputTextareaViewmodel } from './inputTextareaViewmodel';
+import type { InputDateViewmodel } from './inputDateViewmodel';
+import type { InputDateTimeViewmodel } from './inputDateTimeViewmodel';
+import type { InputTimeViewmodel } from './inputTimeViewmodel';
 
 export abstract class UIKitViewmodelsFactory
 {
     abstract createInputText(): InputTextViewmodel;
-    abstract createTextarea(): InputViewmodel<string>;
-    abstract createInputDate(): InputViewmodel<Date | undefined>;
-    abstract createInputTime(): InputViewmodel<number | undefined>;
-    abstract createInputDateTime(): InputViewmodel<Date | undefined>;
+    abstract createTextarea(): InputTextareaViewmodel;
+    abstract createInputDate(): InputDateViewmodel;
+    abstract createInputTime(): InputTimeViewmodel;
+    abstract createInputDateTime(): InputDateTimeViewmodel;
     abstract createButtonIcon(config?: Partial<ButtonIconViewmodelData> & Partial<ButtonIconViewmodelHandlers>): ButtonIconViewmodel;
     abstract createButtonGeneral(config?: Partial<ButtonGeneralViewmodelData> & Partial<ButtonGeneralViewmodelHandlers>): ButtonGeneralViewmodel;
     abstract createGrid<T extends Viewmodel = Viewmodel>(source?: MaybeObservable<T[]>): GridViewmodel<T>;
