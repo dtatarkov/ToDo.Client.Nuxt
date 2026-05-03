624c9085baa4256a368dd00ee14142980b3e1adf
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-05-03 11:10:10 +0300

    added VButtonGeneral

---
+++ app/modules/uikit/components/VButtonGeneral.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { UButton } from '#components';
+import type { ButtonViewmodelColor } from '../types/buttonViewmodelColor';
+
+type VButtonGeneralProps = {
+  title?: string;
+  color?: ButtonViewmodelColor;
+  isDisabled?: boolean;
+  isLoading?: boolean;
+};
+
+// Define emits
+type VButtonGeneralEmits = {
+  (e: 'click'): void;
+};
+
+const props = withDefaults(defineProps<VButtonGeneralProps>(), {
+  title: '',
+  color: 'neutral',
+  isDisabled: false,
+  isLoading: false,
+});
+
+defineEmits<VButtonGeneralEmits>();
+</script>
+
+<template>
+  <UButton
+    :label="props.title"
+    :color="props.color"
+    variant="outline"
+    size="lg"
+    :disabled="props.isDisabled"
+    :loading="props.isLoading"
+    class="cursor-pointer"
+    @click="$emit('click')"
+  />
+</template>
---
+++ app/modules/uikit/stories/VButtonGeneral.stories.ts
@@ -0,0 +1,82 @@
+import type { Meta, StoryObj } from '@nuxtjs/storybook';
+import VButtonGeneral from '../components/VButtonGeneral.vue';
+
+const meta: Meta<typeof VButtonGeneral> = {
+  title: 'UIKit/VButtonGeneral',
+  component: VButtonGeneral,
+
+  render: (args) =>
+  {
+    return {
+      components: { VButtonGeneral },
+
+      setup()
+      {
+        return { args };
+      },
+
+      template: `<VButtonGeneral v-bind="args" />`,
+    };
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    title: 'Primary Button',
+    color: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    title: 'Secondary Button',
+    color: 'secondary',
+  },
+};
+
+export const Neutral: Story = {
+  args: {
+    title: 'Neutral Button',
+    color: 'neutral',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    title: 'Success Button',
+    color: 'success',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    title: 'Warning Button',
+    color: 'warning',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    title: 'Error Button',
+    color: 'error',
+    isDisabled: false,
+    isLoading: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    title: 'Disabled Button',
+    isDisabled: true,
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    title: 'Loading Button',
+    isLoading: true,
+  },
+};
