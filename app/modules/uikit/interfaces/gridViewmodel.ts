04b89b500a9c5e70be2e02c50251f17d8cd99c94
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-05-31 11:21:16 +0300

    removed all uikit/viewmodels except buttons

--- app/modules/uikit/interfaces/gridViewmodel.ts
+++
@@ -1,9 +0,0 @@
-import type { MaybeObservable } from '@/modules/shared/interfaces/maybeObservable';
-import { Viewmodel } from "./viewmodel";
-
-export abstract class GridViewmodel<T extends Viewmodel = Viewmodel> extends Viewmodel<string>
-{
-    abstract get elements(): T[];
-
-    abstract setSource(source: MaybeObservable<T[]>): void;
-}
--- app/modules/uikit/interfaces/toolbarViewmodel.ts
+++
@@ -1,9 +0,0 @@
-import { Viewmodel } from "./viewmodel";
-
-export abstract class ToolbarViewmodel<T extends Viewmodel = Viewmodel> extends Viewmodel
-{
-    abstract get elements(): T[];
-
-    abstract addElement(element: T): void;
-    abstract clear(): void;
-}
