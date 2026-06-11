3feb7a5332149df32e09ce7ba413c0521a24de70
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-06-11 16:57:22 +0300

    removed old classes

--- app/modules/shared/interfaces/callbacksOwner.ts
+++
@@ -1,6 +0,0 @@
-import type { Action } from '../types/action';
-
-export abstract class CallbacksOwner<Callbacks extends Record<string, Action<any[]>>>
-{
-    abstract on(callbacks: Partial<Callbacks>): void;
-}
