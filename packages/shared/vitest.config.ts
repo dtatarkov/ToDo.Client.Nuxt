59366360aa9df478a6ab185d4bfc8e8014ed6b65
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-07-10 11:06:41 +0300

    added nx nuxt plugin

--- packages/shared/vitest.config.ts
+++ packages/shared/vitest.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     name: '@client/shared',
-    include: ['src/**/*.{test,spec}.ts', 'test/**/*.{test,spec}.ts'],
-    exclude: ['node_modules', 'dist', '.git', '.idea', '.vscode'],
+    include: ['test/**/*.{test,spec}.ts'],
     environment: 'node',
     globals: true,
   },
