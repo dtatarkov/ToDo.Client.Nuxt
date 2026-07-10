33ff6b15fd2853e05b4a03b269bf94243e4d261c
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-07-10 19:21:00 +0300

    wired nx tasks with di package

--- packages/di/test/unit/servicesContainer.test.ts
+++ packages/di/test/unit/servicesContainer.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
-import { ServicesContainer } from '../../entities/servicesContainer';
+import { ServicesContainer } from '../../src/entities/servicesContainer';
 
 // Test interfaces
 abstract class ServiceA { }
