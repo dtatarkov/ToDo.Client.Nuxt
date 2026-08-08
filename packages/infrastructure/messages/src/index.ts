c284496274707628d42e5a1fa72a09ad9e63e1f7
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2026-08-08 20:18:52 +0300

    add messageKey type values

--- packages/infrastructure/messages/src/index.ts
+++ packages/infrastructure/messages/src/index.ts
@@ -1,3 +1,4 @@
 export type { MessageKey } from './types/messageKey';
+export { messageKeyValues } from './types/messageKey';
 export { MessagesService } from './services/messagesService';
 export { MessagesServiceImpl } from './services/messagesServiceImpl';
--- packages/infrastructure/messages/src/types/messageKey.ts
+++ packages/infrastructure/messages/src/types/messageKey.ts
@@ -1,20 +1,25 @@
-export type MessageKey = 'todo.field.title.label' |
-    'todo.field.title.placeholder' |
-    'todo.field.title.errors.empty' |
-    'todo.field.description.label' |
-    'todo.field.description.placeholder' |
-    'todo.field.completionDatePlanned.label' |
-    'timeline.showAllNotifications' |
-    'todo.card.completed' |
-    'todo.card.completeBy' |
-    'todo.notification.createError.title' |
-    'todo.notification.updateError.title' |
-    'todo.modal.create.title' |
-    'todo.modal.edit.title' |
-    'button.create' |
-    'button.save' |
-    'button.cancel' |
-    'todos.toolbar.buttons.add' |
-    'entity.field.required' |
-    'entity.field.invalid' |
-    'entity.field.unknown';
+export const messageKeyValues = [
+    'todo.field.title.label',
+    'todo.field.title.placeholder',
+    'todo.field.title.errors.empty',
+    'todo.field.description.label',
+    'todo.field.description.placeholder',
+    'todo.field.completionDatePlanned.label',
+    'timeline.showAllNotifications',
+    'todo.card.completed',
+    'todo.card.completeBy',
+    'todo.notification.createError.title',
+    'todo.notification.updateError.title',
+    'todo.modal.create.title',
+    'todo.modal.edit.title',
+    'button.create',
+    'button.save',
+    'button.cancel',
+    'todos.toolbar.buttons.add',
+    'entity.field.required',
+    'entity.field.invalid',
+    'entity.field.unknown',
+] as const;
+
+export type MessageKey = (typeof messageKeyValues)[number];
+
